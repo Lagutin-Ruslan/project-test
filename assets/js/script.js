1e7dd96f7950aeceef7b9b81d18330631216c1f2
Ruslan Lagutin <r.lagutin@affstar.com>
2025-04-06 21:46:56 +0300

    edits finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -226,7 +226,6 @@ function openMenu() {
     lineTop.classList.add('line__top--open');
     lineBottom.classList.add('line__bottom--open');
     document.body.style.overflow = 'hidden';
-    burgerMnu.style.backgroundColor = "#10101de6";
 
     // Запуск анимации
     requestAnimationFrame(() => {
